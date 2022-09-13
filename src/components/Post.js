ac6a6881487f556feef761c51575f0d4cfe41764
ryandefea <ryan.defea@gmail.com>
2022-09-13 17:43:42 -0500

    add on click
    Co-authored-by: Zhibin Liang  <ifthereisoneday@gmail.com>

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,11 +4,15 @@ import PropTypes from "prop-types";
 const Post = (props) => {
   return(
     <React.Fragment>
-      <h3>Subject: {props.subject}</h3>
-      <h3>Name: {props.name}</h3>
-      <h3>Vote Count: {props.voteCount}</h3>
-      <h3>Comment: {props.comment}</h3>
-      <h3>When: {props.timestamp}</h3>
+      <div onClick={() => {
+        props.whenPostClicked(props.id);
+      }}>
+        <h3>Subject: {props.subject}</h3>
+        <h3>Name: {props.name}</h3>
+        <h3>Vote Count: {props.voteCount}</h3>
+        <h3>Comment: {props.comment}</h3>
+        <h3>When: {props.timestamp}</h3>
+      </div>
     </React.Fragment>
   );
 }
@@ -19,6 +23,7 @@ Post.prototype={
   voteCount: PropTypes.number,
   comment: PropTypes.string,
   timestamp: PropTypes.timestamp,
+  whenPostClicked: PropTypes.func
 }
 
 export default Post;
