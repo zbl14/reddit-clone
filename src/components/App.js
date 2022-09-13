ef12bd5a389cb3fde24bcda7839d90236fe02989
ryandefea <ryan.defea@gmail.com>
2022-09-13 13:15:53 -0500

    add header and control to app
    Co-authored-by: Zhibin Liang  <ifthereisoneday@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header"
+import PostControl from "./PostControl"
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header/>
+      <PostControl/>
+    </React.Fragment>
+  );
+};
+
+export default App;
