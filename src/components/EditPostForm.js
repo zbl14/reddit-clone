766470986736637b2744dbe41d538e2fdd5d4417
ryandefea <ryan.defea@gmail.com>
2022-09-13 17:41:43 -0500

    add in time import and props
    Co-authored-by: Zhibin Liang  <ifthereisoneday@gmail.com>

--- src/components/EditPostForm.js
+++ src/components/EditPostForm.js
@@ -1,12 +1,34 @@
 import React from "react";
 import ReusableForm from "./ReusableForm";
 import PropTypes from "prop-types";
+import { formatDistanceToNow } from "date-fns";
 
-const EditPostForm = () => {
+const EditPostForm = (props) => {
+  const {post} = props;
+  const handleEditPostFormSubmission = (event) =>{
+    event.preventDefault();
+    props.onEditPost({
+      name:event.target.name.value,
+      subject:event.target.subject.value,
+      comment:event.target.comment.value,
+      voteCount: parseInt(event.target.voteCount.value),
+      timestamp: formatDistanceToNow(new Date(), {addSuffix: true}),
+      id: post.id
+    })
+  };
   return(
-    <React.Fragment><h1>form</h1></React.Fragment>
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleEditPostFormSubmission}
+        buttonText="Update"
+      />
+    </React.Fragment>
   );
 };
 
+EditPostForm.propTypes = {
+  post: PropTypes.object,
+  onEditPost: PropTypes.func
+};
 
 export default EditPostForm;
