bf80231c905031cb34ffbcf72ae1caf79926e991
ryandefea <ryan.defea@gmail.com>
2022-09-13 13:16:11 -0500

    create edit post form
    Co-authored-by: Zhibin Liang  <ifthereisoneday@gmail.com>

---
+++ src/components/EditPostForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+
+const EditPostForm = () => {
+  return(
+    <React.Fragment><h1>form</h1></React.Fragment>
+  );
+};
+
+
+export default EditPostForm;
