7ef46c2fb3b35f6dbff4ccdb6e508463864f0474
ryandefea <ryan.defea@gmail.com>
2022-09-13 17:42:23 -0500

    add in imports nad props
    Co-authored-by: Zhibin Liang  <ifthereisoneday@gmail.com>

--- src/components/NewPostForm.js
+++ src/components/NewPostForm.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { v4 } from "uuid";
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
+import { formatDistanceToNow } from "date-fns";
 
-const NewPostForm = () => {
+const NewPostForm = (props) => {
+  const handleNewPostFormSubmission = (event) => {
+    event.preventDefault();
+    props.onNewPostCreation({
+      name: event.target.name.value,
+      subject: event.target.subject.value,
+      comment: event.target.comment.value,
+      voteCount: parseInt(event.target.voteCount.value),
+      timestamp: formatDistanceToNow(new Date(), {addSuffix: true}),
+      id: v4()
+    })
+  }
+
   return(
-    <React.Fragment><h1>form</h1></React.Fragment>
+    <React.Fragment>
+      <ReusableForm
+      formSubmissionHandler={handleNewPostFormSubmission}
+      buttonText= "Post" />
+    </React.Fragment>
   );
+};
+
+NewPostForm.propTypes = {
+  onNewPostCreation: PropTypes.func
 };
 
 export default NewPostForm;
