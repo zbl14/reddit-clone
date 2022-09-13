11e77c9b961059db092cd69e72ce140f678ff917
ryandefea <ryan.defea@gmail.com>
2022-09-13 17:44:49 -0500

    add button clicks
    Co-authored-by: Zhibin Liang  <ifthereisoneday@gmail.com>

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -2,20 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const PostDetail = (props) => {
-  const { post } = props;
+  const { post, onClickingDelete } = props;
   return(
       <React.Fragment>
-      <h3>{post.subject}</h3>
+      <h1>Details of {post.subject}</h1>
       <h3>{post.name}</h3>
       <h3>{post.voteCount}</h3>
       <h3>{post.comment}</h3>
       <h3>{post.timestamp}</h3>
+      <button onClick={props.onClickingEdit}>Update Post</button>
+      <button onClick={() => onClickingDelete(post.id)}>Delete Post</button>
       </React.Fragment>
   );
 }
 
 PostDetail.propTypes = {
-  post: PropTypes.object
+  post: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
 };
 
 export default PostDetail;
