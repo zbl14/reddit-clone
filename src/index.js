23965b7f0415f5f31c0c64ef16d0c73ce6ac7940
ryandefea <ryan.defea@gmail.com>
2022-09-13 17:40:35 -0500

    add imports and store to index
    Co-authored-by: Zhibin Liang  <ifthereisoneday@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import { createStore } from "redux";
+import reducer from "./reducers/post-list-reducer";
+import { Provider } from "react-redux";
 
+const store = createStore(reducer);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
