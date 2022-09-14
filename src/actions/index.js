f5c409c7d6b3b6044254753034f8918d03b958f9
zbl14 <ifthereisoneday@gmail.com>
2022-09-14 12:27:59 -0700

    update addPost

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,24 @@
-import * as c from "./ActionTypes"
+import * as c from "./ActionTypes";
 
 export const addPost = (post) => {
-  const {name, subject, comment, voteCount, timestamp, id} = post;
+  const { name, subject, comment, voteCount, timeOpen, formattedWaitTime, id } =
+    post;
   return {
     type: c.ADD_POST,
     name: name,
     subject: subject,
     comment: comment,
     voteCount: voteCount,
-    timestamp: timestamp,
+    timeOpen: timeOpen,
+    formattedWaitTime: formattedWaitTime,
     id: id,
-  }
-}
+  };
+};
 
 export const deletePost = (id) => ({
   type: c.DELETE_POST,
   id,
-})
+});
 
 export const updateTime = (id, formattedWaitTime) => ({
   type: c.UPDATE_TIME,
